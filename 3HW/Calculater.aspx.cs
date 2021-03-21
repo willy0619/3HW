cf29f40d167a0a9c34b20482fb8aaa45d355a83b
altoliaw <altoliaw@hotmail.com>
2021-03-21 18:17:56 +0800

    計算機 sample

--- 3HW/Calculater.aspx.cs
+++ 3HW/Calculater.aspx.cs
@@ -7,8 +7,140 @@ using System.Web.UI.WebControls;
 
 namespace _3HW {
     public partial class Calculater : System.Web.UI.Page {
+        
+        [SerializableAttribute]
+        public class NumInfo {
+            public List<int> o_List;
+            public int i_Operater;
+            public NumInfo() {
+                o_List = new List<int>();
+                o_List.Add(0);
+                i_Operater = 0;
+            }
+        }
+
+        public NumInfo o_Num = new NumInfo();
+        public int i_Num = 0;
+
         protected void Page_Load(object sender, EventArgs e) {
+            if (ViewState["PreivousInfo"] != null) {
+                o_Num = (NumInfo)ViewState["PreivousInfo"];
+            }
+
+            int i_Ind = (o_Num.o_List.Count -1);
+            i_Num = (o_Num.o_List)[i_Ind];
+            lb_Result.Text = i_Num.ToString();
+        }
+
+        protected void bt_Add_Click(object sender, EventArgs e) {
+            if (o_Num.o_List.Count > 1) {
+                o_Num.o_List[0] += o_Num.o_List[1];
+                o_Num.o_List.RemoveAt(1);
+            }
+            o_Num.i_Operater = 1;
+            lb_Operate.Text = "+";
+            o_Num.o_List.Add(0);
+            ViewState["PreivousInfo"] = o_Num;
+        }
+
+        protected void bt_Miuns_Click(object sender, EventArgs e) {
 
         }
+
+        protected void bt_Product_Click(object sender, EventArgs e) {
+
+        }
+
+        protected void bt_Divide_Click(object sender, EventArgs e) {
+
+        }
+
+        protected void bt_Clear_Click(object sender, EventArgs e) {
+            lb_Operate.Text = "";
+            lb_Result.Text = "0";
+            ViewState["PreivousInfo"] = o_Num = null;
+        }
+
+        protected void bt_Equals_Click(object sender, EventArgs e) {
+            switch (o_Num.i_Operater) {
+                case 1:
+                    if (o_Num.o_List.Count > 1) {
+                        o_Num.o_List[0] += o_Num.o_List[1];
+                        o_Num.o_List.RemoveAt(1);
+                    }
+                    break;
+                default:
+                    break;
+            }            
+            lb_Operate.Text = "";
+            lb_Result.Text = ((o_Num.o_List)[0]).ToString();
+
+            ViewState["PreivousInfo"] = o_Num = null;
+        }
+
+        protected void bt_0_Click(object sender, EventArgs e) {            
+            if (Convert.ToInt32(lb_Result.Text) != 0) {
+                lb_Result.Text += "0";
+                i_Num = Convert.ToInt32(lb_Result.Text);
+                mt_SetInfo();
+            }
+        }
+
+        protected void bt_1_Click(object sender, EventArgs e) {
+            if (Convert.ToInt32(lb_Result.Text) == 0) {
+                lb_Result.Text = "1";
+            }
+            else {
+                lb_Result.Text += "1";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
+        }
+
+        protected void bt_2_Click(object sender, EventArgs e) {
+            if (Convert.ToInt32(lb_Result.Text) == 0) {
+                lb_Result.Text = "2";
+            }
+            else {
+                lb_Result.Text += "2";
+            }
+            i_Num = Convert.ToInt32(lb_Result.Text);
+            mt_SetInfo();
+        }
+
+        protected void bt_3_Click(object sender, EventArgs e) {
+
+        }
+
+        protected void bt_4_Click(object sender, EventArgs e) {
+
+        }
+
+        protected void bt_5_Click(object sender, EventArgs e) {
+
+        }
+
+        protected void bt_6_Click(object sender, EventArgs e) {
+
+        }
+
+        protected void bt_7_Click(object sender, EventArgs e) {
+
+        }
+
+        protected void bt_8_Click(object sender, EventArgs e) {
+
+        }
+
+        protected void bt_9_Click(object sender, EventArgs e) {
+
+        }
+
+        private void mt_SetInfo() {
+            int i_Ind = (o_Num.o_List.Count - 1) ;
+            (o_Num.o_List)[i_Ind] = i_Num;
+            ViewState["PreivousInfo"] = o_Num;
+        }
+
     }
 }
